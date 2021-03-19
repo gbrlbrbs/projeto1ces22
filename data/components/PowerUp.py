72cd15a40076f075f9b3314c2748ed0e37b573d8
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2021-03-19 20:32:01 -0300

    Adds PowerUp class (Not Implemented)

---
+++ data/components/PowerUp.py
@@ -0,0 +1,26 @@
+import random
+
+import pygame
+import data.constants as c
+
+
+class PowerUp:
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = (66, 135, 245)
+        self.randomize_position()
+
+    def randomize_position(self, snake=None):
+        if snake is None:
+            snake = []
+        while True:
+            self.position = (random.randint(0, c.grid_width - 1) *
+                         c.gridsize, random.randint(0, c.grid_height - 1) * c.gridsize)
+            # item position can't override snake's body
+            if self.position not in snake:
+                return
+
+    def draw(self, surface, c1=(93, 216, 228)):
+        r = pygame.Rect((self.position[0], self.position[1]), (c.gridsize, c.gridsize))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, c1, r, 1)
