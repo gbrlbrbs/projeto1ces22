d3ad8a95b82a01a2ec1032235385b1f82fc94d9a
Atsocs <atsocs.cc@gmail.com>
2021-03-21 21:45:29 -0300

    parse maps

--- data/states/MapSelection.py
+++ data/states/MapSelection.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import os
 
 import data.persist_literals as pl
 from data.states.statemachine.GameState import GameState
@@ -10,6 +11,8 @@ class MapSelection(GameState):
         if unlocked_levels is None:
             unlocked_levels = {1}
         self.unlocked_levels = unlocked_levels
+        self._maps = []
+        self._parse_maps()
 
     def startup(self, persistent):
         if pl.new_unlocked_levels in persistent:
@@ -23,3 +26,24 @@ class MapSelection(GameState):
 
     def unlock_levels(self, new_levels):
         self.unlocked_levels = self.unlocked_levels | new_levels
+
+    def _parse_maps(self, filenames=None, extension='.txt'):
+        """
+        Parses resource/maps
+
+        :param filenames: a list of files to parse. Example Usage: filenames=["lvl1", "lvl2"]
+        :param extension: the extension of the files in resources/maps
+        """
+        path = os.path.relpath('resources/maps')
+        if filenames is None:
+            filenames = os.listdir(path)
+        else:
+            filenames = [(f + extension) for f in filenames]
+        for filename in filenames:
+            m = {'name': filename[:-len(extension)], 'free': []}
+            with open('/'.join([path, filename])) as f:
+                for line in f:
+                    line = line.replace('\n', '')
+                    m['free'].append([True if c is '_' else False for c in line])
+            self._maps.append(m)
+        self._maps.sort(key=lambda map_dict: map_dict['name'])
