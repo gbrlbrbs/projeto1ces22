84fb55ee6205103e8fc6356c1d0213ec6242dc91
Atsocs <atsocs.cc@gmail.com>
2021-03-25 22:37:16 -0300

    important bugfix

--- cobrita.py
+++ cobrita.py
@@ -1,7 +1,7 @@
 from data.states import Menu
 
 if __name__ == "__main__":
-    Menu.main()
+    Menu.main(start_map='lvl1')
 
 # TODO:
 #  bugfix I can play lvl 2 even if it is blocked
--- data/states/Menu.py
+++ data/states/Menu.py
@@ -2,6 +2,8 @@ import pygame as pg
 
 import data
 from data.states.statemachine.GameState import GameState
+import data.persist_literals as pl
+import data.state_literals as sl
 
 
 class Menu(GameState):
@@ -17,9 +19,12 @@ class Menu(GameState):
         surface.blit(self.title, self.title_rect)
 
 
-def main():
-    data.main("MENU")
+def main(start_map):
+    ms = data.states[sl.MAP_SELECTION]
+    ms.unlock_map(start_map)
+    persist = {pl.restart: True, pl.selected_map: start_map}
+    data.main("MENU", persist=persist)
 
 
 if __name__ == "__main__":
-    main()
+    main(start_map='lvl1')
